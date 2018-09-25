08f1706b2954e6d4747d7baf75883ceca8f8ab79
Eduardo <ceduardopcoas@gmail.com>
2018-09-25 14:30:11 -0300

    Colocando pecas no tabuleiro

--- Echecs/Program.cs
+++ Echecs/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-
 using tabuleiro;
+using xadrez;
 
 namespace Echecs
 {
@@ -9,6 +9,12 @@ namespace Echecs
         static void Main(string[] args)
         {
             Tabuleiro tab = new Tabuleiro(8, 8);
+
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+            tab.colocarPeca(new Rei (tab, Cor.Preta), new Posicao(2, 4));
+
+
 
             Tela.imprimirTabuleiro(tab);
 
