3303fbe414b73da0a9fb231b54636cab9dff30f4
Carlos Eduardo Pizi Coas <ceduardopcoas@gmail.com>
2018-09-26 05:28:26 -0300

    Continuar Steel

---
+++ Echecs/xadrez/PartidaDeXadrez.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using tabuleiro;
+
+namespace xadrez
+{
+    class PartidaDeXadrez
+    {
+        private Tabuleiro tab;
+        private int turno;
+        private Cor jogadorAtual;
+
+        public PartidaDeXadrez()
+        {
+            tab = new Tabuleiro(8, 8);
+            turno = 1;
+            jogadorAtual = Cor.Branca;
+        }
+
+        public void executaMovimento(Posicao origem, Posicao destino)
+        {
+            Peca p = tab.retirarPeca(origem);
+
+        }
+
+    }
+}
